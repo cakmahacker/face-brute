4ecdd7c14d124394ef5f2c9679043ab5d92798b9
cakma_hack3r <91216396+cakmahacker@users.noreply.github.com>
2021-12-28 00:42:32 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,51 @@
 clear
 echo
-printf "\e[32m[✓]\x1B[33m Lütfen bekleyiniz..."
+printf "\x1B[33m PROJE BAKIMDA"
 echo
-pkg install git
-pkg install php
-git clone https://github.com/car-bomb/hacker
-php hacker.php
+sleep 2
+printf "\x1B[33m DAHA"
+sleep 2
+printf "\x1B[33m  SONRA"
+echo
+sleep 2
+printf "\e[32m[✓]\x1B[33m TEKRAR DENEYEBİLİR"
+echo
+sleep 2
+printf "\e[32m[✓]\x1B[33m Veya Benimle İletişime Geçebiliriniz..."
+echo
+echo
+sleep 3
 clear
-chmod +x face.py
-python2 face.py
+printf "\x1B[37mYönlendiriliyor"
+sleep 1.5
+clear
+printf "\x1B[31mYönlendiriliyor."
+sleep 1.5
+clear
+printf "\x1B[33mYönlendiriliyor.."
+sleep 1.5
+clear
+printf "\x1B[32mYönlendiriliyor..."
+sleep 1.5
+clear
+
+printf "\x1B[31mYönlendiriliyor."
+sleep 1.5
+clear
+printf "\x1B[33mYönlendiriliyor.."
+sleep 1.5
+clear
+printf "\x1B[32mYönlendiriliyor..."
+sleep 1.5
+clear
+
+printf "\x1B[31mYönlendiriliyor."
+sleep 1.5
+clear
+printf "\x1B[33mYönlendiriliyor.."
+sleep 1.5
+clear
+printf "\x1B[32mYönlendiriliyor..."
+clear
+sleep 1.5
+python3 hacker.py
