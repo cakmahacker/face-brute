ff290a9bb2daab534258cc7295311bda076cdb7f
cakma_hack3r <91216396+cakmahacker@users.noreply.github.com>
2021-12-28 15:51:48 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,11 @@
+echo
+printf "\x1B[33mYüklenme tamamlandıktan sonra
+
+1- cd
+2- cd hacker 
+
+Yazınız"
+sleep(3)
 cd
 rm -rf hacker
 git clone https://github.com/cakmahacker/hacker/
-cd
-cd hacker
