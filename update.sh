a73f5ba8ffa0345dbee88f4668533b011cd589cd
cakma_hack3r <91216396+cakmahacker@users.noreply.github.com>
2021-12-28 16:12:04 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,10 +1,18 @@
 echo
-printf "\x1B[33mYüklenme tamamlandıktan sonra
-
+printf "\x1B[33mYüklenme"
+sleep 0.5
+clear
+printf "\x1B[33mYüklenme tamamlandıktan"
+sleep 0.5
+clear
+printf "\x1B[33mYüklenme tamamlandıktan sonra" 
+echo
+sleep 1
+printf "\x1B[33m
 1- cd
 2- cd hacker 
 
-Yazınız"
+Yazınız" 
 sleep 4
 cd
 rm -rf hacker
